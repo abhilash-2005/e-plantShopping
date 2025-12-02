3324c888640acd9625e860ab189aee9ba41f6a83
abhilash kar <abhilashbpd1234@gmail.com>
2025-12-02 21:48:23 +0530

    Add Error Boundary for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import ErrorBoundary from './ErrorBoundary.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
